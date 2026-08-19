6b197872893fc4ef706db1b75eaa01941d966ddf
Vaibhav Patil <vaibhavvpatil132@gmail.com>
2026-08-20 00:24:48 +0530

    feat: implement express routes for comment and reply management

--- server/modules/posts/routes/commentRoutes.js
+++ server/modules/posts/routes/commentRoutes.js
@@ -1,17 +1,17 @@
 import { Router } from 'express';
-import isUserAuth from '../../../middlewares/isUserAuth.js'; 
+import isUserAuth from '../../../middlewares/isUserAuth.js';
 import { createComment, createCommentReply, deleteComment, deleteCommentReply, getComment, getCommentReplies } from '../controllers/commentController.js';
 
 const commentRouter = Router();
 
+// Comments
+commentRouter.get('/:postid', isUserAuth, getComment);
 commentRouter.post('/:postid', isUserAuth, createComment);
 commentRouter.delete('/:postid/:commentid', isUserAuth, deleteComment);
+
+// Comment Replies
+commentRouter.get('/replies/:postid/:commentid', isUserAuth, getCommentReplies);
 commentRouter.post('/replies/:postid/:commentid', isUserAuth, createCommentReply);
 commentRouter.delete('/replies/:postid/:commentid/:replyid', isUserAuth, deleteCommentReply);
-commentRouter.get('/:postid', isUserAuth, getComment); 
-commentRouter.get('/replies/:postid/:commentid', isUserAuth, getCommentReplies);
-
-
-
 
 export default commentRouter; 
