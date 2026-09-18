854fb38d8d9310d59fc1f8dfc945e9b09ff2defe
Vaibhav Patil <vaibhavvpatil132@gmail.com>
2026-09-18 23:37:24 +0530

    feat: add Footer component

--- client/src/components/common/Footer.jsx
+++ client/src/components/common/Footer.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { assets, footer_data } from '../../assets/assets';
 import Logo from './Logo';
-
+import { Heart } from 'lucide-react';
 const Footer = () => {
   return (
     <>
@@ -73,7 +73,7 @@ const Footer = () => {
 
           <div className="max-w-7xl mx-auto mt-6 pt-2 border-t border-neutral-300 flex justify-between items-center z-10 relative">
             <p className="text-neutral-600 text-sm">Copyright © 2025 Experio.</p>
-            <p className="text-sm text-neutral-600">Built with <span className='text-primary'>❤︎</span> by Vaibhav Patil</p>
+            <p className="text-sm text-neutral-600 flex items-center justify-center">Built with <Heart className='w-4 h-4 mx-1 text-primary fill-primary' /> by Vaibhav Patil</p>
           </div>
 
           <div className="relative mt-2 z-0">
